0a64851820aa88b5e76e6756debf03c19906517e
Hunter315 <hunter.mckay.smith@gmail.com>
2018-12-19 17:23:41 -0700

    Styled img and delete image function in profile

--- screens/Profle.js
+++ screens/Profle.js
@@ -1,47 +1,62 @@
-import React from 'react';
-import styles from '../styles';
+import React from "react";
+import styles from "../styles";
 import { connect } from "react-redux";
 import { uploadImages } from "../redux/actions";
 
 import {
-    Text,
-    View,
-    Image,
-    TouchableOpacity
-} from 'react-native';
-
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  ScrollView
+} from "react-native";
 
 class Profile extends React.Component {
-  
-    deleteImage(){
-        this.self.props.dispatch(deleteImage(this.self.props.user.images, this.key))
-    }
+  deleteImage() {
+    this.self.props.dispatch(
+      deleteImage(this.self.props.user.images, this.key)
+    );
+  }
 
-    addImage(){
-        this.props.dispatch(uploadImages(this.props.user.images))
-    }
+  addImage() {
+    this.props.dispatch(uploadImages(this.props.user.images));
+  }
 
-    render() {
-        return(
-            <View>
-                <Text>{this.props.user.id}</Text>
-                <Text>{this.props.user.name}</Text>
-                <Image style={{ width: 75, height: 75 }} source={{uri: this.props.user.photoUrl}} />
-                {this.props.user.images.map((uri, key) => {
-                    return(
-                        <TouchableOpacity key={{key}}>
-                            <Image style={{width: 75, height: 75}}  source={{uri: uri}} />
-                        </TouchableOpacity>
-                    )
-                })}
-            </View>
-        )
-    }
+  render() {
+    return (
+      <ScrollView>
+        <View style={[styles.container, styles.center]}>
+          <View style={styles.container}>
+            <Image
+              style={StyleSheet.img}
+              source={{ uri: this.props.user.photoUrl }}
+            />
+            <Text style={[styles.center, styles.bold]}>
+              {this.props.user.name}
+            </Text>
+          </View>
+          <View style={styles.imgRow}>
+            {this.props.user.images.map((uri, key) => {
+              return (
+                <TouchableOpacity
+                  key={{ key }}
+                  onPress={this.deleteImage.bind({ self: this, key: key })}
+                >
+                  <Image style={styles.img} source={{ uri: uri }} />
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+        </View>
+      </ScrollView>
+    );
+  }
 }
 function mapStateToProps(state) {
-    return {
-      user: state.user
-    };
-  }
-  
-  export default connect(mapStateToProps)(Profile);
+  return {
+    user: state.user
+  };
+}
+
+export default connect(mapStateToProps)(Profile);
